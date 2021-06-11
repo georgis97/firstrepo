bdab5d6430722b58245219435b362f916f85e268
georgis97 <novota.jiri@email.cz>
2021-06-11 22:51:54 +0200

    First project development

---
+++ leaderboard.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('a').click(function(event) {
+        event.preventDefault();
+        $(this).hide("slow");
+    });
+
+    $('#leaderboard').click(function(){
+        $(this).hide("slow");
+    });
+});
---
+++ utmDetector.js
@@ -0,0 +1,7 @@
+if (window.location.href.indexOf("utm") >= 0)
+{
+    console.log("utm is in url")
+}
+else {
+    console.log("utm is not in url")
+}
